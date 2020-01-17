51c9ef6a712e0b53d44feadac3c6f550ba93e1d7
tworoose <51271998+kspaffk@users.noreply.github.com>
2020-01-16 22:16:54 -0700

    add no mat ch

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import bookSearch from "./pages/book-search"
 import bookList from "./pages/book-list"
 import Nav from "./components/nav"
+import nomatch from "./components/nomatch"
 import "./App.css"
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
           <Nav />
           <Switch>
             <Route exact path="/list" component={bookList} />
-            <Route path="*" component={bookSearch} />
+            <Route exact path="/" component={bookSearch} />
+            <Route component={nomatch} />
           </Switch>
         </div>
       </Router>
---
+++ client/src/components/nomatch/index.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+export default function nomatch() {
+  return (
+    <div>
+      404 Error
+    </div>
+  );
+}
