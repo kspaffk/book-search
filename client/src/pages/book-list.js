4975d76e62bd98cdc42280511d46701ef97a829f
Kent Spafford <spafcoding@gmail.com>
2020-01-18 11:29:53 -0700

    fix loop issue with state

--- client/src/pages/book-list.js
+++ client/src/pages/book-list.js
@@ -6,51 +6,55 @@ export default function bookList() {
   const [books, setBooks] = useState([]);
 
   useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const fetchBooks = () => {
     axios.get("/api/books").then(booksDB => {
       setBooks(booksDB.data);
-      console.log(books);
+      console.log(books)
     });
-  }, [books]);
+  };
 
   const renderBooks = () => {
     let bookCards = [];
     if (books.length > 0) {
       bookCards.push(
-      books.map(book => {
-        return (
-          <Book
-            key={book._id}
-            _id={book._id}
-            img={book.image}
-            title={book.title}
-            description={book.description}
-            authors={book.authors}
-            publishedDate={book.publishedDate}
-            link={book.link}
-            button="delete"
-            deleteBook={deleteBook}
-          />
-        );
-      }));
+        books.map(book => {
+          return (
+            <Book
+              key={book._id}
+              _id={book._id}
+              img={book.image}
+              title={book.title}
+              description={book.description}
+              authors={book.authors}
+              publishedDate={book.publishedDate}
+              link={book.link}
+              button="delete"
+              deleteBook={deleteBook}
+            />
+          );
+        })
+      );
     } else {
       bookCards.push(<div key="none">There are no saved books</div>);
     }
     return bookCards;
   };
 
-  const deleteBook = (bookId) => {
-    console.log(bookId)
+  const deleteBook = bookId => {
+    console.log(bookId);
     axios.delete("api/books/" + bookId).then(res => {
-    console.log("BOOK DELETED")
-    })
+      console.log(`BOOK DELETED`);
+      fetchBooks();
+    });
   };
 
   return (
     <div>
       <div className="container">
-        <div className="row">
-          {renderBooks()}
-        </div>
+        <div className="row">{renderBooks()}</div>
       </div>
     </div>
   );
